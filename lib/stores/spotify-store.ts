24d96a36f358f819d37e23ed617156caae710226
igorrsilvaaf <igorqa2022@outlook.com>
2025-05-15 22:19:19 -0300

    feat: Adicionado suporte para playlist do deezer

--- lib/stores/spotify-store.ts
+++ lib/stores/spotify-store.ts
@@ -6,32 +6,37 @@ interface Position {
   y: string;
 }
 
-interface SpotifyState {
+interface MusicPlayerState {
   playlistId: string | null
   contentType: string
+  playerType: 'spotify' | 'deezer'
   isEnabled: boolean
   position: Position | null
   setPlaylistId: (id: string | null) => void
   setContentType: (type: string) => void
+  setPlayerType: (type: 'spotify' | 'deezer') => void
   setIsEnabled: (enabled: boolean) => void
   setPosition: (position: Position) => void
   reset: () => void
 }
 
-export const useSpotifyStore = create<SpotifyState>()(
+export const useSpotifyStore = create<MusicPlayerState>()(
   persist(
     (set) => ({
       playlistId: null,
       contentType: 'playlist',
+      playerType: 'spotify',
       isEnabled: true,
       position: null,
       setPlaylistId: (id) => set({ playlistId: id }),
       setContentType: (type) => set({ contentType: type }),
+      setPlayerType: (type) => set({ playerType: type }),
       setIsEnabled: (enabled) => set({ isEnabled: enabled }),
       setPosition: (position) => set({ position }),
       reset: () => set({ 
         playlistId: null, 
-        contentType: 'playlist', 
+        contentType: 'playlist',
+        playerType: 'spotify',
         isEnabled: false, 
         position: null 
       })
